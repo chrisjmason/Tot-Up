35a07d553ae70f99c517c3ab69b85ebbef978cef
Chris <chrismasonfm@hotmail.co.uk>
2016-09-03 10:05:51 +0100

    Added add category method

--- app/src/main/java/com/tot_up/chris/tot_up/data/db/DbInterface.java
+++ app/src/main/java/com/tot_up/chris/tot_up/data/db/DbInterface.java
@@ -2,10 +2,14 @@ package com.tot_up.chris.tot_up.data.db;
 
 import com.tot_up.chris.tot_up.data.model.Category;
 
+import java.io.IOException;
+import java.sql.SQLDataException;
 import java.util.List;
 
 public interface DbInterface {
 
     List<Category> getCategoryList();
+
+    Void addCategory();
 
 }
