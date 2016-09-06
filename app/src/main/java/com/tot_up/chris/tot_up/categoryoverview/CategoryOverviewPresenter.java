b1c6c74b8bf7694d9a54c1cb45a7fd070f9447bc
Chris <chrismasonfm@hotmail.co.uk>
2016-09-06 13:40:34 +0100

    Set repository schedulers

--- app/src/main/java/com/tot_up/chris/tot_up/categoryoverview/CategoryOverviewPresenter.java
+++ app/src/main/java/com/tot_up/chris/tot_up/categoryoverview/CategoryOverviewPresenter.java
@@ -4,19 +4,23 @@ import com.tot_up.chris.tot_up.base.BasePresenter;
 import com.tot_up.chris.tot_up.data.model.Category;
 import com.tot_up.chris.tot_up.data.repos.OverviewRepositoryInterface;
 
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class CategoryOverviewPresenter extends BasePresenter<CategoryOverviewInterface.View> implements CategoryOverviewInterface.Presenter {
 
     public static final String ADD_SUCCESS_MESSAGE = "Category added";
     public static final String ADD_ERROR_MESSAGE = "Error adding category";
     public static final String DELETE_SUCCESS_MESSAGE = "Category deleted";
     public static final String DELETE_ERROR_MESSAGE = "Error deleting category";
-    public static final String LIST_ERROR_MESSAGE = "Error deleting category";
+    public static final String LIST_ERROR_MESSAGE = "Error retrieving categories";
 
     private OverviewRepositoryInterface repository;
 
     public CategoryOverviewPresenter(CategoryOverviewInterface.View view, OverviewRepositoryInterface repository){
         super.attachView(view);
         this.repository = repository;
+        repository.setSchedulers(Schedulers.io(), AndroidSchedulers.mainThread());
     }
 
     @Override
