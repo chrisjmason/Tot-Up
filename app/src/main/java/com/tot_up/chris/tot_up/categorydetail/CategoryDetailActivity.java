e6758219afb361b0d2a99e451da2b220dfd9879d
chris <chrismasonfm@hotmail.co.uk>
2016-09-29 11:36:58 +0100

    Added UI for category detail

--- app/src/main/java/com/tot_up/chris/tot_up/categorydetail/CategoryDetailActivity.java
+++ app/src/main/java/com/tot_up/chris/tot_up/categorydetail/CategoryDetailActivity.java
@@ -2,24 +2,43 @@ package com.tot_up.chris.tot_up.categorydetail;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
+import com.tot_up.chris.tot_up.Injection;
 import com.tot_up.chris.tot_up.R;
+import com.tot_up.chris.tot_up.categoryoverview.CategoryOverviewActivity;
 import com.tot_up.chris.tot_up.data.model.Expense;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
 public class CategoryDetailActivity extends AppCompatActivity implements CategoryDetailInterface.View {
+    private CategoryDetailAdapter adapter;
+    private CategoryDetailInterface.Presenter presenter;
+    private TextView monthPriceText;
+    private TextView weekPriceText;
+    String categoryName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_detail);
+        categoryName = getIntent().getStringExtra(CategoryOverviewActivity.CATEGORY_NAME);
+        setUpPresenter();
+        setUpUi();
+        getExpenses();
     }
 
     @Override
     public void showExpenses(List<Expense> expenseList) {
-
+        adapter.setExpenseList(expenseList);
     }
 
     @Override
@@ -40,5 +59,54 @@ public class CategoryDetailActivity extends AppCompatActivity implements Categor
     @Override
     public void goToDetail(Expense expense) {
 
+    }
+
+    private void setUpPresenter() {
+        presenter = new CategoryDetailPresenter(this, Injection.provideCategoryDetailRepository());
+    }
+
+    private void setUpUi(){
+        adapter = setUpAdapter();
+        setUpRecyclerView();
+        setUpToolbar();
+        setUpCollapsingToolbar();
+        setUpCollapsingLayout();
+    }
+
+    private CategoryDetailAdapter setUpAdapter(){
+        return new CategoryDetailAdapter(presenter, this);
+    }
+
+    private void setUpRecyclerView(){
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.expense_recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setUpToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.category_detail_toolbar);
+        toolbar.setTitle(categoryName);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setUpCollapsingToolbar(){
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.category_detail_collapsing_toolbar);
+        collapsingToolbar.setTitleEnabled(true);
+        collapsingToolbar.setTitle(categoryName);
+    }
+
+    private void setUpCollapsingLayout(){
+        monthPriceText = (TextView) findViewById(R.id.month_price_text);
+        weekPriceText = (TextView) findViewById(R.id.week_price_text);
+
+        monthPriceText.setText("20.30");
+        weekPriceText.setText("4.50");
+    }
+
+    private void getExpenses(){
+        presenter.getExpenses(categoryName);
     }
 }
---
+++ app/src/main/java/com/tot_up/chris/tot_up/categorydetail/CategoryDetailAdapter.java
@@ -0,0 +1,74 @@
+package com.tot_up.chris.tot_up.categorydetail;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.tot_up.chris.tot_up.R;
+import com.tot_up.chris.tot_up.data.model.Expense;
+
+import java.util.List;
+
+
+public class CategoryDetailAdapter extends RecyclerView.Adapter<CategoryDetailAdapter.ViewHolder> {
+
+    private List<Expense> expenseList;
+    private Context context;
+    private CategoryDetailInterface.Presenter presenter;
+
+    public CategoryDetailAdapter(CategoryDetailInterface.Presenter presenter, Context context) {
+        this.presenter = presenter;
+        this.context = context;
+    }
+
+    @Override
+    public CategoryDetailAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.expense_card, parent, false);
+        return new CategoryDetailAdapter.ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(CategoryDetailAdapter.ViewHolder holder, int position) {
+        CardView cardView = holder.cardView;
+        TextView expensePrice = (TextView) cardView.findViewById(R.id.expense_price_text);
+        TextView expenseDate = (TextView) cardView.findViewById(R.id.expense_date_text);
+        ImageView expenseImage = (ImageView) cardView.findViewById(R.id.expense_image);
+
+        Expense expense = expenseList.get(position);
+
+        expensePrice.setText(expense.getDecimalPrice().toString());
+        expenseDate.setText(expense.getDate());
+
+        Glide.with(context)
+                .load(expense.getImageSrc())
+                .into(expenseImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(expenseList == null){
+            return 0;
+        }
+        return expenseList.size();
+    }
+
+    public void setExpenseList(List<Expense> expenseList){
+        this.expenseList = expenseList;
+        notifyDataSetChanged();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CardView cardView;
+
+        public ViewHolder(CardView cv){
+            super(cv);
+            cardView = cv;
+        }
+    }
+}
