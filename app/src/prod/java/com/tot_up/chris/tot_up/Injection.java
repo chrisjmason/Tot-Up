aa2b24379c5c027b691c1dd4e509f0491750a8ce
chris <chrismasonfm@hotmail.co.uk>
2016-10-12 10:06:16 +0100

    Added Injection for prod flavour

---
+++ app/src/prod/java/com/tot_up/chris/tot_up/Injection.java
@@ -0,0 +1,34 @@
+package com.tot_up.chris.tot_up;
+
+import com.tot_up.chris.tot_up.data.db.DbHelper;
+import com.tot_up.chris.tot_up.data.db.DbInterface;
+import com.tot_up.chris.tot_up.data.repos.categorydetailrepository.CategoryDetailRepository;
+import com.tot_up.chris.tot_up.data.repos.categorydetailrepository.CategoryDetailRepositoryInterface;
+import com.tot_up.chris.tot_up.data.repos.categoryoverviewrepository.CategoryOverviewRepository;
+import com.tot_up.chris.tot_up.util.application.MyApplication;
+
+import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+
+public class Injection {
+    private static Scheduler uiThread = AndroidSchedulers.mainThread();
+    private static Scheduler workThread = Schedulers.io();
+    private static Scheduler mockThread = Schedulers.immediate();
+
+    private Injection(){}
+
+    public static CategoryOverviewRepository provideCategoryOverviewRepository(){
+        return new CategoryOverviewRepository(provideDb(),workThread, uiThread);
+    }
+
+    public static CategoryDetailRepositoryInterface provideCategoryDetailRepository(){
+        return new CategoryDetailRepository(provideDb(), workThread, uiThread);
+    }
+
+    private static DbInterface provideDb(){
+        return new DbHelper(MyApplication.getContext());
+    }
+
+}
