8784467f9824e9dcc23745f0fc504448a76f4b33
chris <chrismasonfm@hotmail.co.uk>
2016-11-22 11:42:56 +0000

    Added total repository

---
+++ app/src/main/java/com/tot_up/chris/tot_up/data/repos/categorytotalrepository/CategoryTotalRepository.java
@@ -0,0 +1,29 @@
+package com.tot_up.chris.tot_up.data.repos.categorytotalrepository;
+
+import com.tot_up.chris.tot_up.data.db.DbInterface;
+import com.tot_up.chris.tot_up.data.model.Category;
+
+import java.util.List;
+
+import rx.Observable;
+import rx.Scheduler;
+
+public class CategoryTotalRepository implements CategoryTotalRepositoryInterface {
+
+    private DbInterface db;
+    private Scheduler worker;
+    private Scheduler ui;
+
+    public CategoryTotalRepository(DbInterface db, Scheduler worker, Scheduler ui) {
+        this.db = db;
+        this.worker = worker;
+        this.ui = ui;
+    }
+
+    @Override
+    public Observable<List<Category>> getCategoryListWithTotals(String totalFromDate) {
+        return Observable.just(db.getCategoryListWithTotals(totalFromDate))
+                .observeOn(ui)
+                .subscribeOn(worker);
+    }
+}
