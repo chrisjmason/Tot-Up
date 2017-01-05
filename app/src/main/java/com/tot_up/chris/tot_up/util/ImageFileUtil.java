d5d1f4e1921899455f0f233ac114bac9aabea5b9
chris <chrismasonfm@hotmail.co.uk>
2017-01-05 10:50:38 +0000

    Changed dir variable to private

--- app/src/main/java/com/tot_up/chris/tot_up/util/ImageFileUtil.java
+++ app/src/main/java/com/tot_up/chris/tot_up/util/ImageFileUtil.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class ImageFileUtil {
 
-    static File totUpDir;
+    private static File totUpDir;
 
     public static File createImageFile(Context context) throws IOException{
         makeTotUpDir(context);
