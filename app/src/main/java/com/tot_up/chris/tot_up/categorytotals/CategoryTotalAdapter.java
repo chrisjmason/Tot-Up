1ad08aa5829896f13d58b6e321df12ab924d9135
chris <chrismasonfm@hotmail.co.uk>
2017-01-05 10:49:31 +0000

    Added category totals page

---
+++ app/src/main/java/com/tot_up/chris/tot_up/categorytotals/CategoryTotalAdapter.java
@@ -0,0 +1,73 @@
+package com.tot_up.chris.tot_up.categorytotals;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.tot_up.chris.tot_up.R;
+import com.tot_up.chris.tot_up.data.model.Category;
+import com.tot_up.chris.tot_up.util.StringFormatterUtil;
+
+import java.util.List;
+
+public class CategoryTotalAdapter extends RecyclerView.Adapter<CategoryTotalAdapter.ViewHolder> {
+
+    private CategoryTotalInterface.Presenter presenter;
+    private Context context;
+    private List<Category> categoryList;
+
+    public CategoryTotalAdapter(CategoryTotalInterface.Presenter presenter, Context context) {
+        this.presenter = presenter;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.totals_card, parent, false);
+        return new CategoryTotalAdapter.ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        CardView cardView = holder.cardView;
+
+        TextView categoryTitle = (TextView) cardView.findViewById(R.id.totals_category_name);
+        TextView categoryTotal = (TextView) cardView.findViewById(R.id.totals_category_total);
+
+        Category category = categoryList.get(position);
+        String categoryTitleString = category.getName();
+        String categoryTotalString = category.getTotal().toString();
+
+        categoryTitle.setText(categoryTitleString);
+        categoryTotal.setText(StringFormatterUtil.addCurrencySignToString(categoryTotalString));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(categoryList == null){
+            return 0;
+        }
+        return categoryList.size();
+    }
+
+    public void setCategoryList(List<Category> categoryList){
+        this.categoryList = categoryList;
+        notifyDataSetChanged();
+    }
+
+    //move this to own class
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CardView cardView;
+
+        public ViewHolder(CardView cv){
+            super(cv);
+            cardView = cv;
+        }
+    }
+
+}
--- app/src/main/java/com/tot_up/chris/tot_up/categorytotals/CategoryTotalsActivity.java
+++ app/src/main/java/com/tot_up/chris/tot_up/categorytotals/CategoryTotalsActivity.java
@@ -2,12 +2,123 @@ package com.tot_up.chris.tot_up.categorytotals;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class CategoryTotalsActivity extends AppCompatActivity {
+import com.tot_up.chris.tot_up.Injection;
+import com.tot_up.chris.tot_up.R;
+import com.tot_up.chris.tot_up.data.model.Category;
+import com.tot_up.chris.tot_up.util.DateUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class CategoryTotalsActivity extends AppCompatActivity implements CategoryTotalInterface.View {
+    CategoryTotalInterface.Presenter presenter;
+    CategoryTotalAdapter adapter;
+    RecyclerView recyclerView;
+    Button weekButton;
+    Button monthButton;
+    Button yearButton;
+    List<Button> buttonList;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_totals);
+        setUpPresenter();
+        setUpUi();
+        presenter.getCategoryListWithTotals(DateUtil.getStartOfWeek());
     }
+
+    @Override
+    public void showTotals(List<Category> categoryList) {
+        adapter.setCategoryList(categoryList);
+    }
+
+    @Override
+    public void showEmpty() {
+
+    }
+
+    @Override
+    public void showErrorMessage(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+    }
+
+    public void setUpPresenter(){
+        presenter = new CategoryTotalPresenter(this, Injection.provideCategoryTotalRepository());
+    }
+
+    public void setUpUi(){
+        setUpAdapter();
+        setUpRecycler();
+        setUpButtons();
+
+    }
+
+    public void setUpAdapter(){
+        adapter = new CategoryTotalAdapter(presenter, getBaseContext());
+    }
+
+    public void setUpRecycler(){
+        recyclerView = (RecyclerView) findViewById(R.id.totals_recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void setUpButtons(){
+        setUpWeekButton();
+        setUpMonthButton();
+        setUpYearButton();
+        buttonList = new ArrayList<>();
+        buttonList.add(weekButton);
+        buttonList.add(monthButton);
+        buttonList.add(yearButton);
+    }
+
+    public void setUpWeekButton(){
+        weekButton = (Button) findViewById(R.id.week_button);
+        buttonPressed(weekButton);
+
+        weekButton.setOnClickListener(v -> {
+            presenter.getCategoryListWithTotals(DateUtil.getStartOfWeek());
+            buttonPressed(weekButton);
+        });
+    }
+
+    public void setUpMonthButton(){
+        monthButton = (Button) findViewById(R.id.week_button);
+        monthButton.setOnClickListener(v -> {
+            presenter.getCategoryListWithTotals(DateUtil.getStartOfMonth());
+            buttonPressed(monthButton);
+        });
+    }
+
+    public void setUpYearButton(){
+        yearButton = (Button) findViewById(R.id.year_button);
+        yearButton.setOnClickListener(v -> {
+            presenter.getCategoryListWithTotals(DateUtil.getStartOfYear());
+            buttonPressed(monthButton);
+        });
+    }
+
+    public void buttonPressed(Button button){
+        button.setBackgroundColor(ContextCompat.getColor(this,R.color.colorGreen));
+        
+        for(Button buttonUnclick:buttonList){
+            if(buttonUnclick!=button){
+                buttonUnclick.setBackgroundColor(ContextCompat.getColor(this,R.color.colorLilac));
+            }
+        }
+    }
+
+
 }
